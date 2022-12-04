2dd972e41abcda290b8a863e213082d9cb4b644a
anthony163 <anthony@techpods.co>
2022-12-05 01:29:43 +0200

    Remove .DS_Store

--- examples/build_examples.js
+++ examples/build_examples.js
@@ -1,9 +1,11 @@
 import { readFileSync, writeFileSync, readdirSync } from 'fs'
 const withRelPath = path => `./examples/${path}`
-const files = readdirSync(withRelPath('programs/')).map(title => ({
-  title,
-  script: readFileSync(withRelPath(`programs/${title}`), 'utf8'),
-}))
+const files = readdirSync(withRelPath('programs/'))
+  .filter(f => f !== '.DS_Store')
+  .map(title => ({
+    title,
+    script: readFileSync(withRelPath(`programs/${title}`), 'utf8'),
+  }))
 writeFileSync(
   withRelPath('snippets.js'),
   `export default ${JSON.stringify(files)}`
